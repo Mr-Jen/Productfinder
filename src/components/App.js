d71298c816f2f6ae62491acefd3e60bcb973b60a
Mr-Jen <jan.ernst1910@gmail.com>
2021-03-23 14:09:59 +0100

    parentData and data replaced by historyStack, that allows to move back and forth in the stack

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,41 @@
 import React, {useState, useEffect} from 'react'
+import styled from 'styled-components'
 //import { getData } from '../shared/api'
+
+const Wrapper = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+`
+
+const BreadCrumbWrapper = styled.div`
+  display: flex:
+`
+
+const BreadCrumb = styled.button`
+  padding: 0;
+  border: none;
+  background: none;
+  color: blue;
+  cursor: pointer;
+`
+
+const ChoiceWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 10px;
+`
+
+const ChoiceButton = styled.button`
+  padding: 5px;
+  margin: 5px;
+`
+
+const Button = styled.button`
+  width: 10%;
+  margin: 2%;
+`
 
 const App = () => {
   useEffect(() => {
@@ -12,49 +48,76 @@ const App = () => {
     return () => { isMounted = false }
   }, [])
 
+  const [historyStack, setHistoryStack] = useState([])
+  const [initData, setInitData] = useState({})
+  const [breadCrumb, setBreadCrumb] = useState([])
+
+  // Adds item to the history stack
   const handleClick = (item) => {
     setHistoryStack(historyStack.concat(item))
+    setBreadCrumb(breadCrumb.concat(item.label))
   }
 
+  // Handles back action and removes the last item from the history stack
   const handleBack = () => {
     const updatedHistory = [...historyStack]
     updatedHistory.pop()
-    //console.log('State before: ', updatedHistory)
     setHistoryStack(updatedHistory)
-    //console.log('State after: ', updatedHistory)
+
+    const updatedBreadCrumb = [...breadCrumb]
+    updatedBreadCrumb.pop()
+    setBreadCrumb(updatedBreadCrumb)
   }
 
-  const [historyStack, setHistoryStack] = useState([])
-  const [initData, setInitData] = useState({})
+  const handleNavigationChange = (crumb) => {
+    console.log("Navigation changed", crumb)
+  }
 
+  // Load the inital data received from the API call
   const loadData = () => {
     setHistoryStack(historyStack.concat(initData))
   }
 
   const latestItem = historyStack[historyStack.length -1]
+  console.log(latestItem)
+
+  console.log('KRÜMEL: ', breadCrumb)
 
   return (
-    <div>
+    <Wrapper>
       <h1>Produktfinder</h1>
       {
-        !latestItem  && <button onClick={() => loadData()}>Start</button>
+        !latestItem  && <Button onClick={() => loadData()}>Start</Button>
       }
+      {/*<p>{breadCrumb}</p>*/}
+      <BreadCrumbWrapper>
+        {        
+          breadCrumb.map((crumb, key) => {
+            return <BreadCrumb onClick={() => handleNavigationChange(crumb)} key={key}>{`${crumb}   -->`}</BreadCrumb>
+          })
+        }
+      </BreadCrumbWrapper>
       {
         latestItem && 
         <div>
           <h2>{latestItem.label}</h2>
           <p>{latestItem.question}</p>
-          <button onClick={() => handleBack()}>Zurück</button>
         </div>
       }
-      {
-        latestItem && latestItem.children.map((item, key) => 
-          <div key={key}>
-            <button onClick={() => handleClick(item)}>{item.label}</button>
-          </div>
-        )
+      <ChoiceWrapper>
+        {
+          //latestItem && latestItem.children.map((item, key) => 
+          latestItem?.children.map((item, key) =>
+            <div key={key}>
+              <ChoiceButton onClick={() => handleClick(item)}>{item.label}</ChoiceButton>
+            </div>
+          )
+        }
+      </ChoiceWrapper>
+      {   
+       latestItem && <Button onClick={() => handleBack()}>Zurück</Button>
       }
-    </div> 
+    </Wrapper> 
   )
 }
 
